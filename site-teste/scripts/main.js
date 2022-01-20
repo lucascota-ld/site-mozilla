fcedf12cddf689c2d047afa287bb75e1d6e66dda
lucascota-id <lucas.a.cota@gmail.com>
2022-01-20 19:04:13 -0300

    integrando-js

---
+++ site-teste/scripts/main.js
@@ -0,0 +1,30 @@
+let minhaImagem = document.querySelector('img')
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+      if(meuSrc === 'imagens/firefox.jpg') {
+      minhaImagem.setAttribute ('src','imagens/firefox2.jpg');
+      } else {
+      minhaImagem.setAttribute ('src','imagens/firefox.jpg');
+    }
+}
+
+var meuButton = document.querySelector('button')
+var meuCabecalho = document.querySelector('h1')
+
+function defineNomeUsuario(){
+  var nomeUsuario = prompt ('Por favor, digite o seu nome!')
+  localStorage.setItem('nome-usuario', nomeUsuario)
+  meuCabecalho.textContent = 'Mozilla é legal, ' + nomeUsuario
+}
+
+if(!localStorage.getItem('nome-usuario')){
+  defineNomeUsuario()
+}else{
+  var nomeSalvo = localStorage.getItem('nome-usuario')
+  meuCabecalho.textContent = 'Mozilla é legal, ' + nomeSalvo
+}
+
+meuButton.onclick = function(){
+  defineNomeUsuario()
+}
